f6e1fa8ad50fa7e31e93de0ed68fba3831046fb2
Panna <pavanksshetty@gmail.com>
2019-04-30 23:42:12 -0700

    Updates

---
+++ precache-manifest.01e7ef2946ca7fdd2224e9b414a86855.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b07fa165f3a18ad8c13223f5ff25997e",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "5d10e7001394be60d43b",
+    "url": "/to-do-list/static/css/main.d5b41b12.chunk.css"
+  },
+  {
+    "revision": "97174ab71286a6f10935",
+    "url": "/to-do-list/static/js/2.20e86c98.chunk.js"
+  },
+  {
+    "revision": "5d10e7001394be60d43b",
+    "url": "/to-do-list/static/js/main.17442049.chunk.js"
+  },
+  {
+    "revision": "15bb3101340a9b514097",
+    "url": "/to-do-list/static/js/runtime~main.9900d293.js"
+  }
+]);
